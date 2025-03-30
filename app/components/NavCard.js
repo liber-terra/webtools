0b2cf981d31d912e743fa9f59e089e256bd2a13c
Leo Wang <leo@192.168.0.103>
2025-03-30 20:58:22 +0800

    home page v1 almost done

---
+++ app/components/NavCard.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { Star } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export default function NavCard({ title, description, url, className }) {
+    return (
+        <div className="flex-center">
+            <Link href={url} className={cn("h-24 w-full max-w-48", className)}>
+                <Card className="h-full w-full py-2 px-4 flex-between gap-0">
+                    <CardTitle className="w-full flex-between">
+                        {title}
+                        <Button variant="ghost" size="icon">
+                            <Star fill="currentColor" />
+                        </Button>
+                    </CardTitle>
+                    <CardDescription className="w-full line-clamp-2">
+                        {description}
+                    </CardDescription>
+                </Card>
+            </Link>
+        </div>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -1,56 +1,56 @@
-import Link from "next/link";
-
+import NavCard from "./components/NavCard";
 // Define tools with their details
 const TOOLS = [
     {
-        name: "计算器",
+        title: "计算器",
         description: "简单实用的计算工具",
-        icon: "/window.svg",
-        href: "/calculator",
-        previewUrl: "/calculator",
+        url: "/calculator",
     },
     {
-        name: "天气查询",
+        title: "天气查询",
         description: "实时天气信息",
-        icon: "/globe.svg",
-        href: "/weather",
-        previewUrl: "/weather",
+        url: "/weather",
     },
     {
-        name: "记事本",
+        title: "记事本",
         description: "快速记录灵感",
-        icon: "/next.svg",
-        href: "/notebook",
-        previewUrl: "/notebook",
+        url: "/notebook",
     },
     {
-        name: "image converter",
-        description: "image converter",
-        icon: "/next.svg",
-        href: "/image-converter",
-        previewUrl: "/image-converter",
+        title: "图片转换",
+        description:
+            "图片转换，就是图片转换，可以转换图片daljdlf   觉得萨里看见啊多少分呢格秒",
+        url: "/image-converter",
+    },
+    {
+        title: "图片裁切",
+        description: "图片裁切",
+        url: "/image-cropperssjksdajlfaj",
+    },
+    {
+        title: "汇率转换",
+        description: "汇率转换",
+        url: "/currency-converter",
+    },
+    {
+        title: "英制转换",
+        description: "英制转换",
+        url: "/unit-converter",
     },
 ];
 
 export default function Home() {
     return (
-        <div className="flex-1 bg-background p-8 sm:p-20 flex flex-col items-center justify-center">
-            <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 w-full max-w-6xl">
+        <main className="flex-center px-6 py-12">
+            <div className="w-full max-w-screen-lg grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 {TOOLS.map((tool) => (
-                    <div key={tool.name} className="">
-                        <Link href={tool.href} className="block relative">
-                            <div className="bg-primary rounded-xl shadow-lg hover:shadow-xl transition-all duration-300 p-6 text-center transform hover:-translate-y-2">
-                                <h2 className="text-xl font-semibold text-gray-800 dark:text-white mb-2">
-                                    {tool.name}
-                                </h2>
-                                <p className="text-gray-600 dark:text-gray-400 text-sm">
-                                    {tool.description}
-                                </p>
-                            </div>
-                        </Link>
-                    </div>
+                    <NavCard
+                        key={tool.title}
+                        {...tool}
+                        className="hover:scale-105 transition-all duration-300"
+                    />
                 ))}
-            </main>
-        </div>
+            </div>
+        </main>
     );
 }
